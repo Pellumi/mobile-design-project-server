17c359b6b9cc85ed36b0bbb112ca1633c93eca55
Pellumi <pellumii67@gmail.com>
2024-10-16 12:11:24 +0100

    added route to save an item and remove the saved item

--- routes/cartRoute.js
+++ routes/cartRoute.js
@@ -61,13 +61,11 @@ router.post("/add-to-cart/:userId", async (req, res) => {
 router.get("/show-cart/:userId", async (req, res) => {
   const userId = req.params.userId;
 
-  if (!userId ) {
-    return res
-      .status(400)
-      .json({ message: "UserId is required" });
+  if (!userId) {
+    return res.status(400).json({ message: "UserId is required" });
   }
 
-  try{
+  try {
     const userCartRef = ref(database, `users/${userId}/cart`);
     const cartSnapshot = await get(userCartRef);
 
@@ -76,11 +74,11 @@ router.get("/show-cart/:userId", async (req, res) => {
     }
 
     const userCart = cartSnapshot.val();
-    res.status(200).json(userCart)
-  } catch (error){
+    res.status(200).json(userCart);
+  } catch (error) {
     res.status(500).json({ message: "Error getting cart: ", error });
   }
-})
+});
 
 router.delete("/remove-from-cart/:userId/:productId", async (req, res) => {
   const { userId, productId } = req.params;
@@ -93,6 +91,11 @@ router.delete("/remove-from-cart/:userId/:productId", async (req, res) => {
 
   try {
     const userCartItemRef = ref(database, `users/${userId}/cart/${productId}`);
+    const cartItemSnapshot = await get(userCartItemRef);
+
+    if (!cartItemSnapshot.exists()) {
+      return res.status(404).json({ message: "Product not found in cart" });
+    }
 
     await remove(userCartItemRef);
     res.status(200).json({
@@ -101,6 +104,80 @@ router.delete("/remove-from-cart/:userId/:productId", async (req, res) => {
   } catch (error) {
     res.status(500).json({
       message: "Error removing item from cart",
+      error: error.message,
+    });
+  }
+});
+
+router.post("/save-item/:userId/:productId", async (req, res) => {
+  const { userId, productId } = req.params;
+
+  if (!userId || !productId) {
+    return res.status(400).json({
+      message: "User ID and Product ID are required.",
+    });
+  }
+
+  try {
+    const productRef = ref(database, `products/${productId}`);
+    const productSnapshot = await get(productRef);
+
+    if (!productSnapshot.exists()) {
+      return res.status(404).json({ message: "Product not found." });
+    }
+
+    const userSavedRef = ref(
+      database,
+      `users/${userId}/savedItems/${productId}`
+    );
+    const savedSnapshot = await get(userSavedRef);
+
+    if (savedSnapshot.exists()) {
+      res.status(404).json({ message: "Item has already been saved" });
+    } else {
+      await set(userSavedRef, {
+        productId,
+      });
+
+      res.status(201).json({ message: "Product saved for later" });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "Error saving item for later",
+      error: error.message,
+    });
+  }
+});
+
+router.delete("/remove-saved-item/:userId/:productId", async (req, res) => {
+  const { userId, productId } = req.params;
+
+  if (!userId || !productId) {
+    return res.status(400).json({
+      message: "User ID and Product ID are required.",
+    });
+  }
+
+  try {
+    const userSavedItemRef = ref(
+      database,
+      `users/${userId}/savedItems/${productId}`
+    );
+    const savedItemSnapshot = await get(userSavedItemRef);
+
+    if (!savedItemSnapshot.exists()) {
+      return res
+        .status(404)
+        .json({ message: "Product not found in savedItems" });
+    }
+
+    await remove(userSavedItemRef);
+    res.status(200).json({
+      message: "Item removed from savedItems successfully!",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error removing item from savedItems",
       error: error.message,
     });
   }
