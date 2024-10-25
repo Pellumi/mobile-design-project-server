74df4bebc773a2f95932369d7e389d95a1cc5b0b
Pellumi <pellumii67@gmail.com>
2024-10-25 13:14:05 +0100

    modified:   db/firebaseConfig.js

--- db/firebaseConfig.js
+++ db/firebaseConfig.js
@@ -3,14 +3,14 @@ import { getDatabase } from "firebase/database";
 import { getStorage } from "firebase/storage";
 import admin from "firebase-admin";
 import dotenv from "dotenv";
-import path from "path";
-import { fileURLToPath } from "url";
-import fs from "fs";
+// import path from "path";
+// import { fileURLToPath } from "url";
+// import fs from "fs";
 
 dotenv.config();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 const firebaseCofig = {
   apiKey: process.env.FIREBASE_API_KEY,
@@ -25,9 +25,11 @@ const firebaseCofig = {
 
 const app = initializeApp(firebaseCofig);
 
-const serviceAccountPath = path.join(__dirname, process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
+// const serviceAccountPath = path.join(__dirname, process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 
-const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, 'utf8'));
+// const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, 'utf8'));
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount), 
   databaseURL: process.env.FIREBASE_DATABASE_URL,
